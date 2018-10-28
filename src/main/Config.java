b1697561ed0b37aa4c94ed497fa1667b6383a8c7
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2018-10-27 22:39:05 -0400

    #25 Config File * Created Config File * Created global constants to be used for the database connection info * Created global constants for the dimensions of the application

---
+++ src/main/Config.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Config {
+	public static final String DB_NAME = "";
+	public static final String DB_USER = "";
+	public static final String DB_PASS = "";
+	
+	public static final int PROG_WIDTH = 1024;
+	public static final int PROG_HEIGHT = 768;
+	
+}
