163a06f4ddeb6668bfe7ad2d9d7a5e89a42f9d8a
Joe Bumbacco <joseph.bumbacco01@stclairconnect.ca>
2018-11-03 15:41:39 -0400

    #5 Log In Page Laout, initialization

--- src/LogInMenu.java
+++ src/LogInMenu.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LogInMenu extends Application {
@@ -7,7 +11,44 @@ public class LogInMenu extends Application {
     	Application.launch();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+	
+	public void start(Stage primaryStage) throws Exception {
+		
+		/*
+		 * This section of the code is what will run after the program has launched and has no existing profiles logged in.
+		 * it will prompt the user for their user name and password and attempt to log them into the database.
+		 */
+		
+		
+		
+		//BorderPane initialization
+		BorderPane pane = new BorderPane();
+		
+		//Creating VBox's for positioning TextFields
+		VBox top = new VBox();
+		top.setAlignment(Pos.CENTER);
+		
+		VBox bottom = new VBox();
+		bottom.setAlignment(Pos.BOTTOM_CENTER);
+		
+		VBox middle = new VBox();
+		middle.setAlignment(Pos.CENTER);
+		
+		VBox baseline = new VBox();
+		baseline.setAlignment(Pos.BASELINE_CENTER);
+		
+		
+		
+		
+		
+
+		Scene scene = new Scene(pane, 1024, 768);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Fit4Life Login");
+		primaryStage.show();
+		
+	}
+
+}
 
     	
