95087d169e0d8bd4c2773285ba1ad67f8ced0e78
Joe Bumbacco <joseph.bumbacco01@stclairconnect.ca>
2018-11-03 15:38:43 -0400

    #5 Log In Page Layout

---
+++ src/LogInMenu.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LogInMenu extends Application {
+    public static void main(String[] args){
+
+    	Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    	
