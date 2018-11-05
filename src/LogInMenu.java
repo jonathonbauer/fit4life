0ca1ebbd8fe1bd730d38ab7c5d926b67ce34b1bb
Joe Bumbacco <joseph.bumbacco01@stclairconnect.ca>
2018-11-05 07:59:19 -0500

    #5 Log In Page Layout - Added in login button and CheckBox to remember users information.

--- src/LogInMenu.java
+++ src/LogInMenu.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -47,19 +49,26 @@ public class LogInMenu extends Application {
 		Text title = new Text("Fit4Life");
 		title.setFont(titleFont);
 		
+		//Adding in Button Nodes
 		
+		Button login = new Button("Log In");
 		
+		//Adding in CheckBox Nodes
+		CheckBox remember = new CheckBox("Remember me?");
+		
+		//Setting default checkbox values
+		remember.setIndeterminate(false);
 		
 		//Adding in TextArea's for both the username and password fields
 		
 		TextArea userName = new TextArea();
 		userName.setMaxHeight(10);
-		userName.setMaxWidth(500);
+		userName.setMaxWidth(300);
 		userName.setText("USERNAME");
 		
 		TextArea passWord = new TextArea();
 		passWord.setMaxHeight(10);
-		passWord.setMaxWidth(500);
+		passWord.setMaxWidth(300);
 		passWord.setText("PASSWORD");
 		
 		/*
@@ -68,8 +77,9 @@ public class LogInMenu extends Application {
 		
 		top.getChildren().add(title);
 		
-		middle.getChildren().addAll(userName, passWord);
+		middle.getChildren().addAll(userName, passWord,remember);
 		
+		bottom.getChildren().add(login);
 		
 		pane.setTop(top);
 		pane.setBottom(bottom);		
