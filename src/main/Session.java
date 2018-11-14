ed42641e11689803615633665b9763581751b56d
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2018-11-14 14:31:11 -0500

    #8 Created Session Class * Created a Session class that will be used to hold session information for the application * Currently its only function is to store the current logged in user

---
+++ src/main/Session.java
@@ -0,0 +1,51 @@
+package main;
+
+import tables.User;
+
+/**
+ * 
+ * This class will be used to hold the active session with the user.
+ * It's primary purpose is to hold the user that is currently logged in,
+ * so that it may be referenced later on in the application.
+ * This also allows us to log the user out when they choose.
+ * This class follows the Singleton Design Pattern.
+ * 
+ *
+ */
+public class Session {
+	
+	private static Session session = null;
+	private User loggedInUser;
+	
+	
+	public Session(User user) {
+		this.loggedInUser = user;
+	}
+	
+	// Get instance method used to create a session with a user at log in
+	public Session getInstance(User user) {
+		if(session == null) {
+			session = new Session(user);
+			return session;
+		} else {
+			return session;
+		}
+	}
+	
+	// Get instance method used to get the instance that has already been created at log in
+	public Session getInstance() {
+		if(session == null) {
+			System.out.println("There is no active session.");
+		}
+		return session;
+	}
+
+	public User getLoggedInUser() {
+		return loggedInUser;
+	}
+
+	public void setLoggedInUser(User loggedInUser) {
+		this.loggedInUser = loggedInUser;
+	}
+	
+}
