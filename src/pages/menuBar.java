3b40576f7c26cc9a9a5e89bf0cef698de5a7a914
Joe Bumbacco <joseph.bumbacco01@stclairconnect.ca>
2018-11-18 11:30:11 -0500

    #23 & #24 - Menubar * initalized the menubar to contain three seprate menus. * file menu contains MenuItems which will all have their own unique purpose * Added eventhandlers to MenuItems to test output

--- src/pages/menuBar.java
+++ src/pages/menuBar.java
@@ -2,6 +2,7 @@ package pages;
 
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 public class menuBar extends MenuBar {
 
@@ -9,15 +10,56 @@ public class menuBar extends MenuBar {
 	private Menu menuEdit;
 	private Menu menuView;
 	
+	private MenuItem exit;
+	private MenuItem logOut;
+	private MenuItem back;
+	private MenuItem settings;
+	private MenuItem credits;
+	
+	
 	public menuBar() {
 		
+		//Adding Values to Menus
 		this.menuFile = new Menu("File");
 		this.menuEdit = new Menu("Edit");
 		this.menuView = new Menu("View");
 		
 		
-		this.menuFile.setOnAction(e->{
-			System.out.println("File Menu Clicked");
+		//Adding Values to MenuItems
+		this.exit = new MenuItem("Exit");
+		this.logOut = new MenuItem("Log Out");
+		this.back = new MenuItem("Back");
+		this.settings = new MenuItem("Settings");
+		this.credits = new MenuItem("Credits");
+		
+		
+		//Adding MenuItems to Menu "File"
+		this.menuFile.getItems().addAll(exit, logOut, back, settings, credits);
+		
+		/* EVENT HANDLERS
+		 *  this represents blocks of code that is only run  once the desired MenuItem is clicked
+		 */
+		
+		//EXIT - Exits the program
+		this.exit.setOnAction(e->{
+			System.out.println("Exit MenuItem Clicked");
+		});
+		
+		//LOGOUT - Brings the user to the login page with no information saved
+		this.logOut.setOnAction(e->{
+			System.out.println("Logout MenuItem Clicked");
+		});
+		//BACK - Returns the user to the previous page
+		this.back.setOnAction(e->{
+			System.out.println("Back MenuItem Clicked");
+		});
+		//SETTINGS - brings user to Settings Menu
+		this.settings.setOnAction(e->{
+			System.out.println("Settings MenuItem Clicked");
+		});
+		//CREDITS - brings user to Credits Menu
+		this.credits.setOnAction(e->{
+			System.out.println("Credits MenuItem Clicked");
 		});
 		
 		this.getMenus().addAll(this.menuFile,this.menuEdit,this.menuView);
