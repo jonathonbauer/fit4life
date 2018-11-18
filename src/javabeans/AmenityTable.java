03ae1e7d3ef453dd3bd7545b6732179bbca81b24
Cody Pollard <codypollard01@stclairconnect.ca>
2018-11-18 15:42:31 -0500

    #67 Created the getAllAmenities and getAmenitys in the AmenityTable

--- src/javabeans/AmenityTable.java
+++ src/javabeans/AmenityTable.java
@@ -1,5 +1,75 @@
 package javabeans;
 
-public class AmenityTable {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import daos.AmenityDAO;
+import database.Database;
+import database.Tables;
+import tables.Amenity;
+
+public class AmenityTable implements AmenityDAO {
+	//database connection
+		Database db = Database.getInstance();
+		ArrayList<Amenity> amenities;
+		Amenity amenity;
+
+	public ArrayList<Amenity> getAllAmenities() {
+		String query = "SELECT * FROM " + Tables.TABLE_AMENITIES;
+		amenities = new ArrayList<Amenity>();
+		
+		try {
+			Statement getAmenities = db.getConnection().createStatement();
+			ResultSet data;
+			data = getAmenities.executeQuery(query);
+			while(data.next()) {
+				amenities.add(new Amenity(data.getInt(Tables.AMENITIES_COLUMN_ID),
+										data.getString(Tables.AMENITIES_COLUMN_AMENITY)
+						));
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} 
+		return amenities;
+	}
+
+	public Amenity getAmenity(int amenityID) {
+		String query = "SELECT * FROM " + Tables.TABLE_AMENITIES + " WHERE "
+				+ Tables.AMENITIES_COLUMN_ID + " = " + amenityID;
+		amenities = new ArrayList<Amenity>();
+		
+		try {
+			Statement getAmenities = db.getConnection().createStatement();
+			ResultSet data;
+			data = getAmenities.executeQuery(query);
+			while(data.next()) {
+				amenities.add(new Amenity(data.getInt(Tables.AMENITIES_COLUMN_ID),
+										data.getString(Tables.AMENITIES_COLUMN_AMENITY)
+						));
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} 
+		return amenity;
+	}
+
+	public void updateAmenity(Amenity amenity) {
+
+		
+	}
+
+	public void deleteAmenity(Amenity amenity) {
+
+		
+	}
+
+	public void createAmenity(Amenity amenity) {
+
+		
+	}
 
 }
