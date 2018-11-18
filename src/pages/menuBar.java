7e97068f7092c04e8054f88cc69578955fbc85fa
Joe Bumbacco <joseph.bumbacco01@stclairconnect.ca>
2018-11-18 11:57:00 -0500

    #24 Menubar - Functionality * Added functional scene switching to MenuItems (Credits, Logout, Settings) * Added functional program exiting to MenuItems (Exit)

--- src/pages/menuBar.java
+++ src/pages/menuBar.java
@@ -1,11 +1,17 @@
 package pages;
 
+import javafx.application.Application;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import main.Main;
 
 public class menuBar extends MenuBar {
 
+	public MainMenu mainMenu;
+	public LogInMenu logInMenu;
+	public Credits creditsPage;
+	
 	private Menu menuFile;
 	private Menu menuEdit;
 	private Menu menuView;
@@ -43,26 +49,69 @@ public class menuBar extends MenuBar {
 		//EXIT - Exits the program
 		this.exit.setOnAction(e->{
 			System.out.println("Exit MenuItem Clicked");
+			System.out.print("Exiting the program...");
+			
+			System.exit(0);
+			
 		});
 		
 		//LOGOUT - Brings the user to the login page with no information saved
 		this.logOut.setOnAction(e->{
 			System.out.println("Logout MenuItem Clicked");
+			System.out.println("Returning to Login Page...");
+			
+			logInMenu = new LogInMenu();
+			Main.mainStage.setScene(logInMenu.getScene());
+			Main.mainStage.setTitle("Fit4Life Member Management");
 		});
 		//BACK - Returns the user to the previous page
 		this.back.setOnAction(e->{
 			System.out.println("Back MenuItem Clicked");
 		});
-		//SETTINGS - brings user to Settings Menu
+		//SETTINGS - Brings user to Settings Menu
 		this.settings.setOnAction(e->{
 			System.out.println("Settings MenuItem Clicked");
+			System.out.println("Heading to Settings Page...");
+			
+			//settingsPage = new Settings();
+			//Main.mainStage.setScene(settingsPage.getScene());
+			//Main.mainStage.setTitle("Fit4Life Member Management");
 		});
-		//CREDITS - brings user to Credits Menu
+		//CREDITS - Brings user to Credits Menu
 		this.credits.setOnAction(e->{
 			System.out.println("Credits MenuItem Clicked");
+			System.out.println("Heading to Credits Page...");
+			
+			creditsPage = new Credits();
+			Main.mainStage.setScene(creditsPage.getScene());
+			Main.mainStage.setTitle("Fit4Life Member Management");
 		});
 		
 		this.getMenus().addAll(this.menuFile,this.menuEdit,this.menuView);
+	}
+	
+	public Credits getCreditsPage() {
+		return creditsPage;
+	}
+
+	public void setCreditsPage(Credits creditsPage) {
+		this.creditsPage = creditsPage;
+	}
+
+	public LogInMenu getLogInMenu() {
+		return logInMenu;
+	}
+
+	public void setLogInMenu(LogInMenu logInMenu) {
+		this.logInMenu = logInMenu;
+	}
+
+	public MainMenu getMainMenu() {
+		return mainMenu;
+	}
+
+	public void setMainMenu(MainMenu mainMenu) {
+		this.mainMenu = mainMenu;
 	}
 
 	public Menu getMenuFile() {
