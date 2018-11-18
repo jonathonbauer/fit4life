8aa758279957242440275716f955ca6e0aa15dda
Cody Pollard <codypollard01@stclairconnect.ca>
2018-11-18 15:22:39 -0500

    #67 Created the DAO for Amenities

--- src/daos/AmenityDAO.java
+++ src/daos/AmenityDAO.java
@@ -1,5 +1,15 @@
 package daos;
 
-public class AmenityDAO {
+import java.util.ArrayList;
+
+import tables.Amenity;
+
+
+public interface AmenityDAO {
+	public ArrayList<Amenity> getAllMembers();
+	public Amenity getMember(int amenityID);
+	public void updateMember(Amenity amenity);
+	public void deleteMember(Amenity amenity);
+	public void createMember(Amenity amenity);
 
 }
