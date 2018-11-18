b419d62816e2115526f07ac0918d22fbe779ef0e
Grant Norris <grant.norris01@stclairconnect.ca>
2018-11-18 14:58:03 -0500

    #65 #66 added the functionality of manager and city tables

--- src/daos/ManagerDAO.java
+++ src/daos/ManagerDAO.java
@@ -1,5 +1,13 @@
 package daos;
+import java.util.ArrayList;
 
-public class ManagerDAO {
+import tables.Manager;
+
+public interface ManagerDAO {
+	public ArrayList<Manager> getAllManagers();
+	public Manager getManager(int managerID);
+	public void updateManager(Manager manager);
+	public void deleteManager(Manager manager);
+	public void createManager(Manager manager);
 
 }
--- src/tabs/CitiesTab.java
+++ src/tabs/CitiesTab.java
@@ -1,11 +1,31 @@
 package tabs;
 
+import main.Session;
+
 public class CitiesTab extends TabTemplate {
 	public static CitiesTab instance = null;
 	
 	private CitiesTab() {
 		this.setText("Cities");
-
+		// Declare the inner tab pane and prevent the tabs from being closed
+		this.tabPane = new TabPane();
+		this.tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+		
+		// If there is a session logged in, then display the CRUD tabs for the users
+		if(Session.isLoggedIn()) {
+			this.tabPane.getTabs().add(ViewCities.getInstance());
+			this.tabPane.getTabs().add(UpdateCitiesTab.getInstance());
+			this.tabPane.getTabs().add(CreateCitiesTab.getInstance());				
+		}
+		
+		// Declare the borderpane and set the inner tabPane to its center
+		this.root = new BorderPane();
+		this.root.setCenter(this.tabPane);
+		
+		// Set the content of the tab to the borderpane
+		this.setContent(root);
+		
+	}
 		
 	}
 	
