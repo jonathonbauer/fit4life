8b4ba27a87a18be51e7c9ecb3ae460a93740583a
Cody Pollard <codypollard01@stclairconnect.ca>
2018-11-21 17:18:19 -0500

    #17 Added Comments to my Code

--- src/tabs/ReportLocationTab.java
+++ src/tabs/ReportLocationTab.java
@@ -37,30 +37,31 @@ public class ReportLocationTab extends Tab {
 	
 	public ReportLocationTab() {
 		
+		//Gets each Location and puts it into an arraylist
 		this.locationTable = new LocationTable();
 		//this.locations = ArrayList<>();
 		this.locations = this.locationTable.getAllLocations();
 		
+		//gets each member and puts it into an arraylist
 		this.memberTable = new MemberTable();
 		this.members = new ArrayList<>();
 		this.members = this.memberTable.getAllMembers();
 		
-		
+		//Creating the bar graph and setting labels
 		this.setText("Location Report");
-		final CategoryAxis XAxis = new CategoryAxis();
-		final NumberAxis YAxis = new NumberAxis();
-		BarChart<String, Number> bc = new BarChart<String, Number>(XAxis, YAxis);
+		CategoryAxis XAxis = new CategoryAxis();
+		NumberAxis YAxis = new NumberAxis();
+		BarChart<String, Number> barChart = new BarChart<String, Number>(XAxis, YAxis);
 		XAxis.setLabel("Location");
 		YAxis.setLabel("Members");
 		
-		
+		//Getting the info to the Chart
 		XYChart.Series chart = new XYChart.Series<>();
 		chart.getData().add(new XYChart.Data(this.locations, this.members));
 
-		
-		
+		//Displaying the info on the screen
 		root = new BorderPane();
-		this.root.setCenter(bc);
+		this.root.setCenter(barChart);
 		this.setContent(root);
 		
 		
