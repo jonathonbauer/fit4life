d21ab56bfc417267bb08118c90f95696333b975d
Jonathon Bauer <jonathon.bauer01@stclairconnect.ca>
2018-12-01 21:01:17 -0500

    #97 Table JavaDoc comments * Added JavaDoc comments to all of the Table objects

--- src/javabeans/AmenityTable.java
+++ src/javabeans/AmenityTable.java
@@ -10,6 +10,11 @@ import database.Database;
 import database.Tables;
 import tables.Amenity;
 
+/**
+ * This class is used to hold all CRUD functionality for the Amenity table in
+ * the database
+ *
+ */
 public class AmenityTable implements AmenityDAO {
 
 	// database connection
@@ -17,7 +22,11 @@ public class AmenityTable implements AmenityDAO {
 	ArrayList<Amenity> amenities;
 	Amenity amenity;
 
-	@Override
+	/**
+	 * This method is used to retrieve all Amenities in the database
+	 * 
+	 * @return An arraylist of Amenity objects
+	 */
 	public ArrayList<Amenity> getAllAmenities() {
 		// TODO Auto-generated method stub
 		String query = "SELECT * FROM " + Tables.TABLE_AMENITIES;
@@ -38,7 +47,14 @@ public class AmenityTable implements AmenityDAO {
 		return amenities;
 	}
 
-	@Override
+	/**
+	 * This method is used to get a single Amenity using its ID
+	 * 
+	 * @param amenityID
+	 *            is the ID of the amenity we wish to retrieve
+	 * 
+	 * @return A single Amenity object
+	 */
 	public Amenity getAmenity(int amenityID) {
 		// TODO Auto-generated method stub
 		String query = "SELECT * FROM " + Tables.TABLE_AMENITIES + " WHERE " + Tables.AMENITIES_COLUMN_ID + " = "
@@ -59,7 +75,13 @@ public class AmenityTable implements AmenityDAO {
 		return amenity;
 	}
 
-	@Override
+	/**
+	 * This method is used to update an amenity in the database
+	 * 
+	 * @param amenity
+	 *            an Amenity object to be updated
+	 * 
+	 */
 	public void updateAmenity(Amenity amenity) {
 		// TODO Auto-generated method stub
 		String query = "UPDATE " + Tables.TABLE_AMENITIES + " SET " + Tables.AMENITIES_COLUMN_AMENITY + " = '"
@@ -72,7 +94,13 @@ public class AmenityTable implements AmenityDAO {
 		}
 	}
 
-	@Override
+	/**
+	 * This method is used to delete an amenity out of the database
+	 * 
+	 * @param amenity
+	 *            an Amenity object to be deleted
+	 * 
+	 */
 	public void deleteAmenity(Amenity amenity) {
 		// TODO Auto-generated method stub
 		String query = "DELETE FROM " + Tables.TABLE_AMENITIES + " WHERE " + Tables.AMENITIES_COLUMN_ID + " = "
@@ -86,7 +114,13 @@ public class AmenityTable implements AmenityDAO {
 
 	}
 
-	@Override
+	/**
+	 * This method is used to insert an amenity in the database
+	 * 
+	 * @param amenity
+	 *            the Amenity object to be created
+	 * 
+	 */
 	public void createAmenity(Amenity amenity) {
 		// TODO Auto-generated method stub
 		String query = "INSERT INTO " + Tables.TABLE_AMENITIES + "(" + Tables.AMENITIES_COLUMN_AMENITY + ") VALUES ('"
